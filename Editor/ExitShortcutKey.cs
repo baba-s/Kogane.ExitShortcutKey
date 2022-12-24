29a495c96fb52ecad39bcc8435006ed862a965ec
baba-s <shotababa1206@gmail.com>
2022-12-24 16:17:04 +0900

    add

---
+++ Editor/ExitShortcutKey.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR_WIN
+
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class ExitShortcutKey
+    {
+        [Shortcut( "Kogane/Exit", KeyCode.Q, ShortcutModifiers.Action )]
+        private static void Exit()
+        {
+            EditorApplication.Exit( 0 );
+        }
+    }
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ExitShortcutKey
+# Kogane Exit Shortcut Key
+
+Windows で Ctrl + Q を押して Unity エディタを終了できるようにするエディタ拡張
